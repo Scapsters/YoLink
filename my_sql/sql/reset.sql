ff9576004edc5077c53754b8c85f77c2e4769cfb
Scapsters <scotty.happy@gmail.com>
2024-12-28 20:18:50 -0500

    fix weird sql syntax idk man

--- my_sql/sql/reset.sql
+++ my_sql/sql/reset.sql
@@ -1,3 +1,3 @@
-DROP TABLE IF EXISTS `%(schema_name)s`.`data`
-DROP TABLE IF EXISTS `%(schema_name)s`.`events`
-DROP TABLE IF EXISTS `%(schema_name)s`.`devices`
+DROP TABLE IF EXISTS `%(schema_name)s`.`data`;
+DROP TABLE IF EXISTS `%(schema_name)s`.`events`;
+DROP TABLE IF EXISTS `%(schema_name)s`.`devices`;
